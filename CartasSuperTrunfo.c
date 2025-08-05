750fa23abe74693af021221bbfaeb551006d2c7b
brayanlineker <brayanlineker@gmail.com>
2025-08-05 02:55:51 +0000

    Novo Commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,13 +10,95 @@ int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
+    // Carta 1
+    char estado1;
+    char codigo1[5];
+    char nome1[20];
+    int populacao1;
+    float area1;
+    float pib1;
+    int ptoturistico1;
+
+    // Carta 2
+    char estado2;
+    char codigo2[5];
+    char nome2[20];
+    int populacao2;
+    float area2;
+    float pib2;
+    int ptoturistico2;
+
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     
+    printf("Cadastro da Carta 1:\n");
+    printf("Nome do Estado (A-B): ");
+    scanf(" %c", &estado1);
+
+    printf("Código da carta (ex: A1): ");
+    scanf("%s", codigo1);
+
+    printf("Nome da cidade: ");
+    scanf("%s", nome1);
+
+    printf("População: ");
+    scanf("%d", &populacao1);
+
+    printf("Área: ");
+    scanf("%f", &area1);
+
+    printf("PIB: ");
+    scanf("%f", &pib1);
+
+    printf("Número de pontos turísticos: ");
+    scanf("%d", &ptoturistico1);
+
+    printf("\nCadastro da Carta 2:\n");
+    printf("Nome do Estado (A-B): ");
+    scanf(" %c", &estado2);
+
+    printf("Código da carta (ex: A2): ");
+    scanf("%s", codigo2);
+
+    printf("Nome da cidade: ");
+    scanf("%s", nome2);
+
+    printf("População: ");
+    scanf("%d", &populacao2);
+
+    printf("Área: ");
+    scanf("%f", &area2);
+
+    printf("PIB: ");
+    scanf("%f", &pib2);
+
+    printf("Número de pontos turísticos: ");
+    scanf("%d", &ptoturistico2);
+
+
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+
+    printf("\nCarta 1:\n");
+    printf("Estado: %c\n", estado1);
+    printf("Código: %s\n", codigo1);
+    printf("Cidade: %s\n", nome1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
+    printf("Pontos Turísticos: %d\n", ptoturistico1);
+
+    printf("\nCarta 2:\n");
+    printf("Estado: %c\n:", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Cidade: %s\n", nome2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("Pontos Turísticos: %d\n", ptoturistico2);
+
 
     return 0;
 }
